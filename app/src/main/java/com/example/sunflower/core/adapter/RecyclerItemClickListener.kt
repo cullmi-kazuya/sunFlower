1b2fd96f581477096376ceafc78cdf5c7b28ecf5
kazuyamakida <nrtgntm@gmail.com>
2024-01-09 23:21:37 +0900

    recycleView に ClickListener を追加する

---
+++ app/src/main/java/com/example/sunflower/core/adapter/RecyclerItemClickListener.kt
@@ -0,0 +1,43 @@
+package com.example.sunflower.core.adapter
+
+import android.content.Context
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerItemClickListener(
+    context: Context,
+    recyclerView: RecyclerView,
+    private val listener: OnItemClickListener?
+): RecyclerView.OnItemTouchListener  {
+
+    interface OnItemClickListener {
+        fun onItemClick(view: View, position: Int)
+    }
+
+    private val gestureDetector: GestureDetector
+
+    init {
+        gestureDetector = GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onSingleTapUp(e: MotionEvent): Boolean {
+                return true
+            }
+        })
+    }
+
+    override fun onInterceptTouchEvent(recyclerView: RecyclerView, e: MotionEvent): Boolean {
+        val child = recyclerView.findChildViewUnder(e.x, e.y)
+        if (child != null && listener != null && gestureDetector.onTouchEvent(e)) {
+            listener.onItemClick(child, recyclerView.getChildAdapterPosition(child))
+            return true
+        }
+        return false
+    }
+
+    override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {
+    }
+
+    override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
+    }
+}
--- app/src/main/java/com/example/sunflower/home/gallery/ui/GalleryFragment.kt
+++ app/src/main/java/com/example/sunflower/home/gallery/ui/GalleryFragment.kt
@@ -2,6 +2,7 @@ package com.example.sunflower.home.gallery.ui
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.sunflower.R
+import com.example.sunflower.core.adapter.RecyclerItemClickListener
 import com.example.sunflower.customCell.data.FruitCellItem
 import com.example.sunflower.customCell.ui.CustomCellAdapter
 import org.json.JSONArray
@@ -49,8 +51,16 @@ class GalleryFragment : Fragment() {
 
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerList)
         recyclerView.adapter = CustomCellAdapter(itemList)
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
+        recyclerView.layoutManager =
+            GridLayoutManager(requireContext(), 2, RecyclerView.VERTICAL, false)
 
+        recyclerView.addOnItemTouchListener(RecyclerItemClickListener(requireContext(), recyclerView,
+            object: RecyclerItemClickListener.OnItemClickListener {
+                override fun onItemClick(view: View, position: Int) {
+                    Log.i("sample", "click ${position}")
+                }
+            })
+        )
         return view
     }
 }
