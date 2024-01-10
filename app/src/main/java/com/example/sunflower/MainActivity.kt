3f17cd2691e006fe713f0e225ec04f6a25d30974
kazuyamakida <nrtgntm@gmail.com>
2024-01-10 20:25:42 +0900

    navigation で画面遷移できるようにする

--- app/src/main/java/com/example/sunflower/MainActivity.kt
+++ app/src/main/java/com/example/sunflower/MainActivity.kt
@@ -2,18 +2,11 @@ package com.example.sunflower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.sunflower.home.HomeTabFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeTabFragment())
-                .commit()
-        }
     }
 }
